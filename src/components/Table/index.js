2bedd6cb6cf8a5527e1837ad81b19d74c60c9a7b
RakelO <zvarun4ik@ukr.net>
2017-12-20 15:20:33 +0200

    transtion rework fox

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -27,7 +27,7 @@ export default class Table extends Component {
 
         return (
             <div className = { Styles.table }>
-                <table>
+                <table border = '1'>
                     <thead>
                         <tr>
                             <td>#</td>
